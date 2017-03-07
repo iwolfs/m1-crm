4db5e8602e5db360c286552b3ae496e1aca032f9
bestvist <18610776607@sina.cn>
2017-03-07 16:14:47 +0800

    api

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,7 @@
  * Created by Administrator on 2017/3/7.
  */
 const base = 'http://m1backapi_t.m1world.com';
-
-let API = {
+export default {
     //dashboard 新增用户
     DASH_NEW_USER: base + '/newuser/statistics',
     //dashboard 用户登录次数
@@ -11,5 +10,3 @@ let API = {
     //dashboard 新增page
     DASH_NEW_PAGE: base + '/newpage/statistics'
 };
-
-window.API = API;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import App from './App';
-import './config';
+import API from './config/api';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';    // 默认主题
 // import '../static/css/theme-green/index.css';       // 浅绿色主题
+window.API = API;
 
 Vue.use(ElementUI);
 new Vue({
